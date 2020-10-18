d5bd19db45e811916acac6ddf953d15e2683d610
rzkytmgr <rzkytmgr@gmail.com>
2020-10-18 17:34:17 +0700

    fix(APIURL): add url for API

--- api/getGombal.js
+++ api/getGombal.js
@@ -1,20 +1,22 @@
 const axios = require("axios");
 
+const APIURL = "https://api-neraka.vercel.app/api/";
+
 exports.getGombal = async () => {
-    const getWords = await axios.get("https://api-neraka.vercel.app/api/bucin");
+    const getWords = await axios.get(APIURL + "bucin");
     const result = getWords.data.result;
     const { kata } = result;
     return kata;
 };
 
 exports.getGay = async () => {
-    const getPercentage = await axios.get("https://api-neraka.vercel.app/api/gay");
+    const getPercentage = await axios.get(APIURL + "gay");
     const result = getPercentage.data.result;
     return result;
 };
 
 exports.getJodoh = async () => {
-    const getPercentage = await axios.get("https://api-neraka.vercel.app/api/jodoh");
+    const getPercentage = await axios.get(APIURL + "jodoh");
     const result = getPercentage.data.result;
     return result;
 };
